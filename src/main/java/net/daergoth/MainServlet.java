f4cef75d849bae557b5dc6e77ddbc5dc63ecd64e
István Lakatos <atyep95@msn.com>
2016-06-27 16:04:18 +0200

    Added UTF8 support and name parameter reading

---
+++ src/main/java/net/daergoth/MainServlet.java
@@ -0,0 +1,24 @@
+package net.daergoth;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public MainServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		response.getWriter().append(request.getParameter("name") + " űáéúőóüöí!");
+	} 
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
